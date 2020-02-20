18c504689a348a564bb70ec1c193397abe791dd4
Daniel Crook <daniel.crook@st-aidans.com>
2020-02-20 12:54:29 +0000

    add simpel z spread calulations to analytics mod

--- src/bonds/analytics.rs
+++ src/bonds/analytics.rs
@@ -4,77 +4,60 @@ use crate::equations::{df};
 use crate::dates::{calc_payment_dates, year_fract_to_date};
 use crate::rates::{YieldCurveFactory, YieldCurve, HasQueryRate};
 
-// use std::f64::consts;
-
-
-fn ytm_2(val_dt : Date<Utc>, mat : f64, freq : f64, coupon : f64, price : f64) -> f64 {
-
-    let maturities = vec![6., 12., 18., 24., 30., 36.];
-    // 
-    let maturities : Vec<f64> = maturities.into_iter().map(|x| x/12.).collect();
+fn ytm_and_zspread(val_dt : Date<Utc>, mat : f64, freq : f64, coupon : f64, price : f64) -> (f64, f64) {
     let maturities = vec![1., 2., 3., 4., 5., 6.]; 
     let p = 100.;
     // bond price
-    let b = 100.;
-    let b = 116.8;
     let b = 144.81; // looking for 2% yld and 0% cpn
     let b = 90.47; //looking for 7% yield 5% cpn   
-    let couponRate = 0.05;
-    // let couponRate = 5.0;
-    
-    // calculate future cashflows
-    //let cashflows : Vec<(f64, f64)> = maturities.into_iter().map(|i| ( i, ((p * couponRate) / 2.0)).collect();
-    
-    //let mut grid: [[i32; 5]; 2] = [[5; 5]; 2];
-    
-    // let mut cashflows : [[f64; 6]; 2] =  [[5.; 6]; 2];
-    // println!("Before {:?}", cashflows);
-    
-    let mut cashflows : [f64; 6] =  [couponRate; 6];
-    let mut yearFrac : [f64; 6] =  [5.; 6];
-
-    // double[] cashflows = maturities.Select(i => (P * couponRate) / 2.0).ToArray();
-    
+    let mut cashflows : [f64; 6] =  [0.; 6];
+    let mut year_fracts : [f64; 6] =  [5.; 6];
 
     for (i, row) in  maturities.into_iter().enumerate(){
-        yearFrac[i] = row;
-        cashflows[i] = (p * couponRate) / 1.0;
+        year_fracts[i] = row;
+        cashflows[i] = (p * coupon/100.) / 1.0;
     };
     cashflows[5] = cashflows[5] + 100.; 
 
-    println!("yearFrac {:?}", yearFrac);
+    let val_dt = Utc::now().date();
+    let mat_dt = year_fract_to_date(val_dt, 6.); //Hacky maturity date for annual
+    let freq = 1;
+
+    //Assuming start of bond life, no payment yet
+    let pay_dates = calc_payment_dates(val_dt, None, mat_dt, freq);
+
+    println!("year_fracts {:?}", year_fracts);
+    println!("Pay Dates {:?}", pay_dates);
     println!("Cashflows {:?}", cashflows);
-    
-    let initialGuess = couponRate;
-    CalculateYield(initialGuess, &cashflows, &yearFrac, b)
+
+    let init_guess = coupon/100.;
+    let yc = YieldCurveFactory::create_default_curve();
+    // let yc = YieldCurveFactory::create_flat_curve(1.);
+    let yld = calculate_yield(init_guess, &cashflows, &year_fracts, b) * 100.;
+    let zspread = calculate_z_spread(init_guess, &cashflows, &pay_dates, &year_fracts, b, &yc) * 10000.;
+
+    (yld, zspread)
 }
 
-fn CalculateYield(initialGuess : f64, cashflows : &[f64], yearFrac : &[f64], B : f64) -> f64 {
+// Solve for yield using Newton-Raphson method
+fn  calculate_yield(init_guess : f64, cashflows : &[f64], year_fracts : &[f64], B : f64) -> f64 {
         
     let error : f64 = 0.000000001;
-    let mut x_i : f64 = initialGuess-1.0;
-    let mut x_i_next : f64 = initialGuess;
+    let mut x_i : f64 = init_guess-1.0;
+    let mut x_i_next : f64 = init_guess;
 
-    println!("Initial: PV={}, initial_guess={}, x_i={}, x_i_next={}", B, initialGuess, x_i, x_i_next);
+    println!("Initial: PV={}, initial_guess={}, x_i={}, x_i_next={}", B, init_guess, x_i, x_i_next);
     while (x_i_next - x_i).abs() > error {
         x_i = x_i_next;
-        // linq's Zip is handy to perform a sum over expressions involving several arrays
-        // numerator = cashflows.Zip(yearFrac, (x,y) => (x * Math.Exp(y *-1*x_i))).Sum();
-        // denominator = yearFrac.Zip(cashflows, (x,y) => (x*y*Math.Exp(x*-1*x_i))).Sum();
-        let mut numerator = -1.;
-        let mut denominator = -1.;
         let mut nums : Vec<f64> = vec![];
         let mut denoms: Vec<f64>  = vec![];
-
-
-        
-        for it in cashflows.iter().zip(yearFrac.iter()) {
+ 
+        for it in cashflows.iter().zip(year_fracts.iter()) {
             //cf, year fract
             let (cf, y) = it;
             
             // let n = x * std::f64::consts::E.powf(y *-1.*x_i);
             // let d = y * x *std::f64::consts::E.powf(y *-1.*x_i);
-
             let n = cf * df(x_i, *y);
             let d = y * cf * df(x_i, *y);
 
@@ -82,17 +65,53 @@ fn CalculateYield(initialGuess : f64, cashflows : &[f64], yearFrac : &[f64], B :
             denoms.push(d);
         }
 
-        numerator = nums.iter().sum();
-        denominator = denoms.iter().sum();
+        let numerator : f64 = nums.iter().sum();
+        let denominator : f64 = denoms.iter().sum();
         x_i_next = x_i + (numerator - B) / denominator;
-        println!("xi+1=xi+(n-B)/d === {} = {} + ({} - {})/{}", x_i_next, x_i, numerator, B, denominator);
+        println!("xi+1=xi+(n-B)/d === {}={}+({}-{})/{}", x_i_next, x_i, numerator, B, denominator);
+    }
+    x_i_next
+}
+
+// Solve for Z-spread using Newton-Raphson method
+fn calculate_z_spread(init_guess : f64, cashflows : &[f64], pay_dates : &Vec<Date<Utc>>, year_fracts : &[f64], B : f64, yc : &YieldCurve) -> f64 {
+        
+    let error : f64 = 0.000000001;
+    let mut x_i : f64 = init_guess-1.0;
+    let mut x_i_next : f64 = init_guess;
+    
+    println!("Initial: PV={}, initial_guess={}, x_i={}, x_i_next={}", B, init_guess, x_i, x_i_next);
+    while (x_i_next - x_i).abs() > error {
+        x_i = x_i_next;
+        let mut nums : Vec<f64> = vec![];
+        let mut denoms : Vec<f64>  = vec![];
+ 
+        let  mut pd_iter = pay_dates.iter();
+        for it in cashflows.iter().zip(year_fracts.iter()) {
+            //cf, year fract
+            let (cf, y) = it;
+            let dt = pd_iter.next();
+            let rate = yc.queryRate(*dt.unwrap()) / 100.; //correct scale
+            
+            let n = cf * df(x_i + rate, *y);
+            let d = y * cf * df(x_i + rate, *y);
+
+            println!("cf={} y={} dt={} rate={} n(guess)={}", cf, y, *dt.unwrap(), rate, x_i);
+
+            nums.push(n);
+            denoms.push(d);
+        }
+
+        let numerator : f64 = nums.iter().sum();
+        let denominator : f64 = denoms.iter().sum();
+        x_i_next = x_i + (numerator - B) / denominator;
+        // println!("xi+1=xi+(n-B)/d === {}={}+({}-{})/{}", x_i_next, x_i, numerator, B, denominator);
     }
     x_i_next
 }
 
 
-
-fn yld_calced(val_dt : Date<Utc>, mat : f64, freq : f64, coupon : f64, price : f64) -> f64 {
+fn calculate_yield_naive_solver(val_dt : Date<Utc>, mat : f64, freq : f64, coupon : f64, price : f64) -> f64 {
     let mat_dt = year_fract_to_date(val_dt, mat); //Hacky maturity date for annual
     //Assuming start of bond life, no payment yet
     let pay_dates = calc_payment_dates(val_dt, None, mat_dt, freq as u32);
@@ -187,14 +206,15 @@ mod tests {
     }
 
     #[test]
-    fn bonds_analytics_ytm_2() {
+    fn bonds_analytics_ytm_zspread() {
         let val_dt = Utc::now().date();
-        let mat = 5.;
+        let mat = 6.;
         let freq = 2.;
-        let coupon = 3.;
+        let coupon = 2.;
         let price = 100.;
     
-        println!("Yld for bond is {}", ytm_2(val_dt, mat, freq, coupon, price));
+        let (yld, zspread) = ytm_and_zspread(val_dt, mat, freq, coupon, price);
+        println!("Yld={}, Z-Spread={}", yld, zspread);
     }
 
     #[test]
@@ -205,16 +225,15 @@ mod tests {
         let coupon = 3.;
         let price = 100.;
     
-        println!("Yld for bond is {}", yld_calced(val_dt, mat, freq, coupon, price));
+        println!("Yld for bond is {}", calculate_yield_naive_solver(val_dt, mat, freq, coupon, price));
         
         let coupon = 6.;
         let price = 113.83327682778175;
     
-        println!("Yld for bond is {}", yld_calced(val_dt, mat, freq, coupon, price));
+        println!("Yld for bond is {}", calculate_yield_naive_solver(val_dt, mat, freq, coupon, price));
             
-        let coupon = 2.;
-        let price = 113.83327682778175;
-    
+        // let coupon = 2.;
+        // let price = 113.83327682778175;
         //This will run forever
         //println!("Yld for bond is {}", yld_calced(val_dt, mat, freq, coupon, price));
     } 
